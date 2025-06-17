fbdced61c6cbb19678338a6f26d6375ec67fb79e
AJ <jellaarunkumar@gmail.com>
2025-06-17 11:00:29 +1000

    added space

--- tests/Login.test.js
+++ tests/Login.test.js
@@ -6,5 +6,7 @@ test('Login with valid credentials', async ({ page }) =>
   await page.fill('#login-username', 'HLSSNCOM288292');
   await page.fill('#login-password', 'Load12');
   await page.locator('#login-submit-button').click();
+
+  
   await expect(page).toHaveURL('https://stage.highlowmi.dev/my-account/dashboard');
 });
