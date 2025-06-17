1f1f0792b7134026c83d42ce8018d22a70382134
AJ <jellaarunkumar@gmail.com>
2025-06-17 10:53:28 +1000

    new commit 17/6

--- tests/Login.test.js
+++ tests/Login.test.js
@@ -3,7 +3,6 @@ import { test, expect } from '@playwright/test';
 test('Login with valid credentials', async ({ page }) => 
 {
   await page.goto('https://wastage.highlowmi.dev/login');
-
   await page.fill('#login-username', 'HLSSNCOM288292');
   await page.fill('#login-password', 'Load12');
   await page.locator('#login-submit-button').click();
